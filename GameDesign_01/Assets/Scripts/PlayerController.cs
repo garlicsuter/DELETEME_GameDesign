dffac82850ccf6c2b2d7d307d6ae9adf3249f439
Adam Calvelage <23CalvA@live.elida.k12.oh.us>
2020-11-06 10:43:55 -0500

    Added my initals to debug log

--- GameDesign_01/Assets/Scripts/PlayerController.cs
+++ GameDesign_01/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Mr. Suter was here");
+        Debug.Log("AC was here");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
