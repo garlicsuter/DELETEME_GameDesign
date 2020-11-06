9fdbbbf043d9732a4b61805623227729f9bdcee8
jierredansby <70708418+jierredansby@users.noreply.github.com>
2020-11-06 10:47:37 -0500

    Change made by jie
    i added a debug

--- GameDesign_01/Assets/Scripts/PlayerController.cs
+++ GameDesign_01/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Debug.Log("Mr. Suter was here");
+        Debug.Log("Jie");
     }
 
     // Update is called once per frame
