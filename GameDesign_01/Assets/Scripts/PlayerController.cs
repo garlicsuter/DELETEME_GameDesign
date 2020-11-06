db8f183274fec20cf040fe8e30c81917619919f4
23connd <70708424+23connd@users.noreply.github.com>
2020-11-06 10:47:30 -0500

    Scriptedit
    script was edited

--- GameDesign_01/Assets/Scripts/PlayerController.cs
+++ GameDesign_01/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Debug.Log("Mr. Suter was here");
+        Debug.Log("Dylan C. how has yor day benn so far");
     }
 
     // Update is called once per frame
