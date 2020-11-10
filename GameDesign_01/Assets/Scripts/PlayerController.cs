1f6d653801741cf6f7abbf9943a615b503e4c962
ThineAcoustic <70708679+ThineAcoustic@users.noreply.github.com>
2020-11-10 10:32:59 -0500

    Playercontroller Update
    added a debug signature

--- GameDesign_01/Assets/Scripts/PlayerController.cs
+++ GameDesign_01/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Debug.Log("Mr. Suter was here 11/10");
-
+        Debug.Log("DB was here 11/10");
     }
 
     void Update()
